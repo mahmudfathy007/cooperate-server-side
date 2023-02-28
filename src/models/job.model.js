bab59c1cf2b2f63e3e27ed8b28e566258071cb18
Mahmoud fathy <modyy.fathyy@gmail.com>
2023-02-28 19:44:08 +0200

    feat
    added user model
     //require some changes

---
+++ src/models/job.model.js
@@ -0,0 +1,46 @@
+const { number } = require('joi');
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: true
+    },
+    client_id: {
+        type :number,
+        required: true,
+    },
+    payment_type: {
+        type: String,
+        required: [true, 'Please Choose your payment method'],
+        enum: ['online', 'Fawry', 'paypal']
+    },
+    project_length: {
+        type : date,
+        required: true
+    },
+    category_id: {
+        type: number,
+        required: true
+    },
+    experiance_level: {
+        type: String,
+        required: true
+    },
+    budjet: {
+        type: number,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    }
+
+
+
+});
+
+const Job = mongoose.model('Job', jobSchema);
+
+module.exports = Job;
